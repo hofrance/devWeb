13df41cd09e5af2c6b69b6d74f9654fc08d6fbd0
hofrance <105118876+hofrance@users.noreply.github.com>
2024-01-25 22:25:46 +0100

    Delete MonPortfolio directory

--- MonPortfolio/blog.php
+++
@@ -1,8 +0,0 @@
-<?php include 'head.php'; ?>
-
-    <?php include 'navigation.php'; ?>
-
-    <img src="./img/1.png" alt="coming soon" class="coming-soon-image">
-
-   
-<?php include 'footer.php'; ?>

37d22af771ccd28f77b0fedc78b97286e56fdd40
hofrance <hofrancebankouezi@gmail.com>
2024-02-01 16:30:55 +0100

    ajout des lien vers mon site officielle

--- index.php
+++ index.php
@@ -24,12 +24,10 @@
             <h1 class="display-4">Richard BANKOUEZI</h1>
             <h2 class="font-weight-normal">Etudiant passionné en Cybersécurité et Systèmes Réseau</h2>
             <div class="header-cta my-2">
-
                 <a href="https://www.richard-hofrance.com" target="_blank" class="btn btn-primary btn-lg">En savoir
                     plus</a>
-                
-            </a>
-                <a  href="http://richard-hofrance.com/contact" target="_blank" rel="noopener noreferrer" class="btn btn-outline-light btn-lg">Contactez-moi</a>
+                <a href="http://richard-hofrance.com/contact" target="_blank" rel="noopener noreferrer"
+                    class="btn btn-outline-light btn-lg">Contactez-moi</a>
             </div>
             <div class="social-icons">
                 <a href="https://www.linkedin.com/in/richard-hofrance" target="_blank" class="text-white mx-2"><i
@@ -45,66 +43,26 @@
     <main class="container my-5">
         <!-- Section À Propos de Moi -->
         <section class="about-me mb-4">
-            <h3 class="mb-3">À Propos de Moi</h3>
-            <p class="lead">Je suis Richard BANKOUEZI, étudiant en cybersécurité et systèmes réseau, avec une passion
-                profonde pour la technologie et l'innovation.</p>
-
-            <p>Depuis mon plus jeune âge, je suis fasciné par les ordinateurs et la manière dont ils peuvent être
-                utilisés pour résoudre des problèmes complexes. Cette fascination m'a mené à choisir un parcours
-                académique axé sur la cybersécurité, où j'ai pu développer une solide compréhension des principes
-                fondamentaux de la sécurité des réseaux et des systèmes informatiques.</p>
-
-            <p>En plus de mes études, je suis activement impliqué dans plusieurs projets personnels et communautaires,
-                où je mets en pratique mes compétences en codage, en sécurité réseau, et en développement de logiciels.
-                J'ai un intérêt particulier pour les solutions de sécurité qui protègent les données et la vie privée
-                des utilisateurs.</p>
-
-            <p>Au-delà de l'informatique, je suis un musicien passionné et un membre actif de la communauté artistique
-                de mon université. Cette dimension créative me permet de voir les problèmes sous un angle différent et
-                d'apporter des solutions innovantes dans mon domaine d'étude.</p>
-
-            <p>Je suis déterminé à continuer à apprendre et à évoluer dans le domaine de la cybersécurité, et je suis
-                toujours à la recherche de nouvelles opportunités pour développer mes compétences et contribuer à des
-                projets significatifs.</p>
+            <!-- Contenu de la section À Propos de Moi -->
         </section>
-
 
         <!-- Section Formation -->
         <section class="education mb-4">
-
-            <a href="http://richard-hofrance.com" target="_blank" rel="noopener noreferrer">
-                <h3>Formation </h3>
-            </a>
-
             <!-- Contenu de la section Formation -->
         </section>
 
         <!-- Section Compétences -->
         <section class="skills mb-4">
-
-            <a href="http://richard-hofrance.com/competences" target="_blank" rel="noopener noreferrer">
-                <h3>Compétences</h3>
-
-            </a>
             <!-- Contenu de la section Compétences -->
         </section>
 
         <!-- Section Projets -->
         <section class="projects mb-4">
-            <a href="http://richard-hofrance.com/project" target="_blank" rel="noopener noreferrer">
-                <h3>Projets</h3>
-
-            </a>
-
             <!-- Contenu de la section Projets -->
         </section>
 
         <!-- Section Contact -->
         <section class="contact mb-4">
-
-            <a href="http://richard-hofrance.com/contact" target="_blank" rel="noopener noreferrer">
-                <h3>Contact</h3>
-            </a>
             <!-- Contenu de la section Contact -->
         </section>
     </main>

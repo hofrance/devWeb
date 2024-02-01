0abaea1765da74b81e2c4ac5ad29797032217ad7
hofrance <hofrancebankouezi@gmail.com>
2024-02-01 15:44:25 +0100

    ajout des lien vers mon site officielle

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Découvrez le portfolio de Richard BANKOUEZI, spécialisé en cybersécurité et systèmes réseau. Explorez mes projets, compétences et expériences en informatique.">
-    <meta name="keywords" content="Richard BANKOUEZI, Cybersécurité, Systèmes Réseau, Informatique, Technologie, Portfolio">
+    <meta name="description"
+        content="Découvrez le portfolio de Richard BANKOUEZI, spécialisé en cybersécurité et systèmes réseau. Explorez mes projets, compétences et expériences en informatique.">
+    <meta name="keywords"
+        content="Richard BANKOUEZI, Cybersécurité, Systèmes Réseau, Informatique, Technologie, Portfolio">
     <meta name="author" content="Richard BANKOUEZI">
     <title>Portfolio de Richard BANKOUEZI - Cybersécurité et Systèmes Réseau</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
@@ -22,13 +25,17 @@
             <h2 class="font-weight-normal">Etudiant passionné en Cybersécurité et Systèmes Réseau</h2>
             <div class="header-cta my-2">
 
-                <a href="https://www.richard-hofrance.com" target="_blank" class="btn btn-primary btn-lg">En savoir plus</a>
+                <a href="https://www.richard-hofrance.com" target="_blank" class="btn btn-primary btn-lg">En savoir
+                    plus</a>
                 <a href="#contact" class="btn btn-outline-light btn-lg">Contactez-moi</a>
             </div>
             <div class="social-icons">
-                <a href="https://www.linkedin.com/in/richard-hofrance" target="_blank" class="text-white mx-2"><i class="fab fa-linkedin"></i></a>
-                <a href="https://github.com/hofrance" target="_blank" class="text-white mx-2"><i class="fab fa-github"></i></a>
-                <a href="https://www.youtube.com/@richard-hofrance" target="_blank" class="text-white mx-2"><i class="fab fa-youtube"></i></a>
+                <a href="https://www.linkedin.com/in/richard-hofrance" target="_blank" class="text-white mx-2"><i
+                        class="fab fa-linkedin"></i></a>
+                <a href="https://github.com/hofrance" target="_blank" class="text-white mx-2"><i
+                        class="fab fa-github"></i></a>
+                <a href="https://www.youtube.com/@richard-hofrance" target="_blank" class="text-white mx-2"><i
+                        class="fab fa-youtube"></i></a>
             </div>
         </div>
     </header>
@@ -36,45 +43,66 @@
     <main class="container my-5">
         <!-- Section À Propos de Moi -->
         <section class="about-me mb-4">
-    <h3 class="mb-3">À Propos de Moi</h3>
-    <p class="lead">Je suis Richard BANKOUEZI, étudiant en cybersécurité et systèmes réseau, avec une passion profonde pour la technologie et l'innovation.</p>
-    
-    <p>Depuis mon plus jeune âge, je suis fasciné par les ordinateurs et la manière dont ils peuvent être utilisés pour résoudre des problèmes complexes. Cette fascination m'a mené à choisir un parcours académique axé sur la cybersécurité, où j'ai pu développer une solide compréhension des principes fondamentaux de la sécurité des réseaux et des systèmes informatiques.</p>
-    
-    <p>En plus de mes études, je suis activement impliqué dans plusieurs projets personnels et communautaires, où je mets en pratique mes compétences en codage, en sécurité réseau, et en développement de logiciels. J'ai un intérêt particulier pour les solutions de sécurité qui protègent les données et la vie privée des utilisateurs.</p>
+            <h3 class="mb-3">À Propos de Moi</h3>
+            <p class="lead">Je suis Richard BANKOUEZI, étudiant en cybersécurité et systèmes réseau, avec une passion
+                profonde pour la technologie et l'innovation.</p>
 
-    <p>Au-delà de l'informatique, je suis un musicien passionné et un membre actif de la communauté artistique de mon université. Cette dimension créative me permet de voir les problèmes sous un angle différent et d'apporter des solutions innovantes dans mon domaine d'étude.</p>
+            <p>Depuis mon plus jeune âge, je suis fasciné par les ordinateurs et la manière dont ils peuvent être
+                utilisés pour résoudre des problèmes complexes. Cette fascination m'a mené à choisir un parcours
+                académique axé sur la cybersécurité, où j'ai pu développer une solide compréhension des principes
+                fondamentaux de la sécurité des réseaux et des systèmes informatiques.</p>
 
-    <p>Je suis déterminé à continuer à apprendre et à évoluer dans le domaine de la cybersécurité, et je suis toujours à la recherche de nouvelles opportunités pour développer mes compétences et contribuer à des projets significatifs.</p>
-</section>
+            <p>En plus de mes études, je suis activement impliqué dans plusieurs projets personnels et communautaires,
+                où je mets en pratique mes compétences en codage, en sécurité réseau, et en développement de logiciels.
+                J'ai un intérêt particulier pour les solutions de sécurité qui protègent les données et la vie privée
+                des utilisateurs.</p>
+
+            <p>Au-delà de l'informatique, je suis un musicien passionné et un membre actif de la communauté artistique
+                de mon université. Cette dimension créative me permet de voir les problèmes sous un angle différent et
+                d'apporter des solutions innovantes dans mon domaine d'étude.</p>
+
+            <p>Je suis déterminé à continuer à apprendre et à évoluer dans le domaine de la cybersécurité, et je suis
+                toujours à la recherche de nouvelles opportunités pour développer mes compétences et contribuer à des
+                projets significatifs.</p>
+        </section>
 
 
         <!-- Section Formation -->
         <section class="education mb-4">
-            
-                <a href="http://richard-hofrance.com" target="_blank" rel="noopener noreferrer"><h3>Formation   </h3></a>
-         
+
+            <a href="http://richard-hofrance.com" target="_blank" rel="noopener noreferrer">
+                <h3>Formation </h3>
+            </a>
+
             <!-- Contenu de la section Formation -->
         </section>
 
         <!-- Section Compétences -->
         <section class="skills mb-4">
-            <h3>Compétences</h3>
-            <a href="http://richard-hofrance.com/competences" target="_blank" rel="noopener noreferrer"><h3>Formation   </h3></a>
+
+            <a href="http://richard-hofrance.com/competences" target="_blank" rel="noopener noreferrer">
+                <h3>Compétences</h3>
+
+            </a>
             <!-- Contenu de la section Compétences -->
         </section>
 
         <!-- Section Projets -->
         <section class="projects mb-4">
-        <a href="http://richard-hofrance.com/projects" target="_blank" rel="noopener noreferrer"><h3>Formation   </h3></a>
-            <h3>Projets</h3>
+            <a href="http://richard-hofrance.com/projects" target="_blank" rel="noopener noreferrer">
+                <h3>Projets</h3>
+
+            </a>
+
             <!-- Contenu de la section Projets -->
         </section>
 
         <!-- Section Contact -->
         <section class="contact mb-4">
-            <h3>Contact</h3>
-            <a href="http://richard-hofrance.com/contact" target="_blank" rel="noopener noreferrer"><h3>Formation   </h3></a>
+
+            <a href="http://richard-hofrance.com/contact" target="_blank" rel="noopener noreferrer">
+                <h3>Contact</h3>
+            </a>
             <!-- Contenu de la section Contact -->
         </section>
     </main>
@@ -87,4 +115,5 @@
 
     <script src="script.js"></script>
 </body>
+
 </html>
